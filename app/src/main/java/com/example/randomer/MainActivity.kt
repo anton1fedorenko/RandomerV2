206e8120f89f5e9b75967608193b4135a9150972
Anton Fedorenko <Antonf_spb@mail.ru>
2023-09-10 17:50:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/randomer/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.randomer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val seekBarNum = findViewById<TextView>(R.id.seekBarNum)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBarNum.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        rollButton.setOnClickListener {
+            var rand = Random.nextInt(seekBar.progress+1)
+            if (rand == 0){
+                rand++
+            }else {
+                resultsTextView.text = rand.toString()
+            }
+        }
+    }
+}
