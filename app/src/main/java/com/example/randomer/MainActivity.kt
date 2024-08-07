5157505807009530ecf5a5ff4be95feaedb98644
Anton <64866888+fedorenkovich@users.noreply.github.com>
2024-08-07 22:17:42 +0300

    07.08.24

--- app/src/main/java/com/example/randomer/MainActivity.kt
+++ app/src/main/java/com/example/randomer/MainActivity.kt
@@ -1,44 +1,72 @@
 package com.example.randomer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
-import android.widget.SeekBar
+import android.widget.EditText
 import android.widget.TextView
-import kotlin.random.Random
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton = findViewById<Button>(R.id.rollButton)
+        //declaration of values by setting them a value of displayed palette
         val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
-        val seekBar = findViewById<SeekBar>(R.id.seekBar)
-        val seekBarNum = findViewById<TextView>(R.id.seekBarNum)
+        val quantity = findViewById<EditText>(R.id.quantity)
+        val rangeMin = findViewById<EditText>(R.id.rangeMin)
+        val rangeMax = findViewById<EditText>(R.id.rangeMax)
+        val rollButton = findViewById<Button>(R.id.rollButton)
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                seekBarNum.text = progress.toString()
-            }
+        resultsTextView.movementMethod = ScrollingMovementMethod() //adding scroll movement to a TextView
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {
+        //adding displayable hints for the EditText palettes
+        quantity.setHint("1")
+        rangeMin.setHint("1")
+        rangeMax.setHint("10")
 
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-
-            }
-
-        })
-
+        //actions when "Roll" button is pressed
         rollButton.setOnClickListener {
-            var rand = Random.nextInt(seekBar.progress+1)
-            if (rand == 0){
-                rand++
-            }else {
-                resultsTextView.text = rand.toString()
+            resultsTextView.text = "" //clearing a TextView after every time the button is pressed
+
+            //checking if EditView is not empty. displaying error message with Toast
+            if (quantity.text.toString().isNullOrEmpty() || rangeMin.text.toString().isNullOrEmpty() || rangeMax.text.toString().isNullOrEmpty()){
+                Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            var quantityList : ArrayList<String> = arrayListOf() //list for random generated numbers
+            //converting EditView for Editable to Int
+            var min : Int = rangeMin.text.toString().toInt()
+            var max : Int = rangeMax.text.toString().toInt()
+            var quantity : Int = quantity.text.toString().toInt()
+
+            //checking if range is not inverted
+            if (min > max){
+                Toast.makeText(this, "Range set wrong", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //generating "quantity" of random numbers in a range from "rangeMin" to "rangeMax" + adding them to the list
+             for (i in 0..quantity){
+                var rand = (min..max).random()
+                quantityList.add(rand.toString())
+            }
+
+            //displaying list in TextView
+            for(i in 0 until quantity){
+                if (i == quantity - 1){
+                    resultsTextView.append(quantityList[i])
+                }else{
+                    resultsTextView.append("${quantityList[i]} ")
+                }
+            }
+
+
+
         }
     }
 }
